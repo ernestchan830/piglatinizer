36339454971bc265985a413b559501720209a6c9
ernestchan830 <aloeicecream@gmail.com>
2019-10-16 01:29:24 +0000

    add vowelCheck function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 $( "document" ).ready(function() {
 
+$("#button").click(function() {
+    var inputValue = $('.input').val();
+    $('output').text(inputValue)
+    console.log(inputValue)
+    var vowels = ["a","e","i","o","u"];
+    var firstLetter = inputValue[0];
 
+    if (vowels.includes(firstLetter)){
+        $('output').text('start with vowel');
+
+     }
+
+        $('output').text('start with consonant');
+
+})
 
 });
+
+
+//     function sliceFromSecond(word) {
+//      return word.slice(1);
+
+// }
+
+//     function firstLetterAy(word) {
+//      return word[0] + "ay";
+// }
+
+// function checkVowel(word) {
+//   var vowels = ["a","e","i","o","u"];
+//   var firstLetter = word[0];
+
+//   if (vowels.includes(firstLetter)){
+//     return 'start with vowel';
+
+//   }
+//   return 'start with consonant';
+
+// }
